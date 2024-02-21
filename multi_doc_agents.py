cddb7b204c5853db8c2ae8eb5447fca59ad35cfd
raoulbia <raoulbia.ai@gmail.com>
2024-02-21 18:38:07 +0000

    Load and process documents, build agents, and define custom retrievers and object retrievers.

--- multi_doc_agents.py
+++ multi_doc_agents.py
@@ -1,7 +1,8 @@
 import nest_asyncio
 nest_asyncio.apply()
-
-import os, json
+import asyncio
+from tqdm import tqdm
+import os, json, pickle
 from pathlib import Path
 # from streamlit import caching
 import streamlit as st
@@ -36,6 +37,8 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.objects import ObjectIndex, SimpleToolNodeMapping
 from llama_index.agent.openai_legacy import FnRetrieverOpenAIAgent
 
+from llama_index.core import Document
+
 load_dotenv(find_dotenv(), override=True)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -56,21 +59,23 @@ for file_path in data_dir_path.glob('*.pdf'):
 @st.cache_resource
 def load_documents(titles):
     docs = []
-    for idx, title in enumerate(titles):
+    for idx, d in enumerate(titles):
         try:
-            docs.append(SimpleDirectoryReader(
-                input_files=[f"{dir_path}/{title}.pdf"]
-            ).load_data())
-            print(f"Successfully loaded document: {title}")
-        except Exception as e:
-            print(f"Error loading document: {title}. Error: {str(e)}")
-    return docs
+            doc = SimpleDirectoryReader(
+                input_files=[f"{dir_path}/{d}.pdf"]
+            ).load_data()
 
-st.session_state.docs_loaded = False
-if 'docs_loaded' not in st.session_state:
-    st.session_state.docs = load_documents(titles)
-    st.session_state.docs_loaded = True
-docs = st.session_state.docs
+            loaded_doc = Document(
+                text="\n\n".join([d.get_content() for d in doc]),
+                metadata={"path": str(d)},
+            )
+            # print(loaded_doc.metadata["path"])
+            docs.append(loaded_doc)
+
+            print(f"Successfully loaded document: {d}")
+        except Exception as e:
+            print(f"Error loading document: {d}. Error: {str(e)}")
+    return docs
 
 
 Settings.llm = OpenAI(model="gpt-3.5-turbo")
@@ -79,10 +84,10 @@ Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
 async def build_agent_per_doc(nodes, file_base):
     print(file_base)
 
-    vi_out_path = f"./data/llamaindex_docs/{file_base}"
-    summary_out_path = f"./data/llamaindex_docs/{file_base}_summary.pkl"
+    vi_out_path = f"persist/{file_base}"
+    summary_out_path = f"persist/{file_base}_summary.pkl"
     if not os.path.exists(vi_out_path):
-        Path("./data/llamaindex_docs/").mkdir(parents=True, exist_ok=True)
+        Path("persist/").mkdir(parents=True, exist_ok=True)
         # build vector index
         vector_index = VectorStoreIndex(nodes)
         vector_index.storage_context.persist(persist_dir=vi_out_path)
@@ -131,7 +136,7 @@ async def build_agent_per_doc(nodes, file_base):
     ]
 
     # build agent
-    function_llm = OpenAI(model="gpt-4")
+    function_llm = OpenAI(model="gpt-3.5-turbo")
     agent = OpenAIAgent.from_tools(
         query_engine_tools,
         llm=function_llm,
@@ -169,10 +174,95 @@ async def build_agents(docs):
 
     return agents_dict, extra_info_dict
 
+docs = load_documents(titles)
+x`x`
 
-agents_dict, extra_info_dict = asyncio.run(build_agents(docs))
+
+# define tool for each document agent
+all_tools = []
+for file_base, agent in agents_dict.items():
+    summary = extra_info_dict[file_base]["summary"]
+    doc_tool = QueryEngineTool(
+        query_engine=agent,
+        metadata=ToolMetadata(
+            name=f"tool_{file_base}",
+            description=summary,
+        ),
+    )
+    all_tools.append(doc_tool)
+
+    llm = OpenAI(model_name="gpt-3.5-turbo")
+
+    tool_mapping = SimpleToolNodeMapping.from_objects(all_tools)
+    obj_index = ObjectIndex.from_objects(
+        all_tools,
+        tool_mapping,
+        VectorStoreIndex,
+    )
+    vector_node_retriever = obj_index.as_node_retriever(similarity_top_k=10)
 
 
+    # define a custom retriever with reranking
+    class CustomRetriever(BaseRetriever):
+        def __init__(self, vector_retriever, postprocessor=None):
+            self._vector_retriever = vector_retriever
+            self._postprocessor = postprocessor or CohereRerank(top_n=5)
+            super().__init__()
+
+        def _retrieve(self, query_bundle):
+            retrieved_nodes = self._vector_retriever.retrieve(query_bundle)
+            filtered_nodes = self._postprocessor.postprocess_nodes(
+                retrieved_nodes, query_bundle=query_bundle
+            )
+
+            return filtered_nodes
+
+
+    # define a custom object retriever that adds in a query planning tool
+    class CustomObjectRetriever(ObjectRetriever):
+        def __init__(self, retriever, object_node_mapping, all_tools, llm=None):
+            self._retriever = retriever
+            self._object_node_mapping = object_node_mapping
+            self._llm = llm or OpenAI("gpt-3.5-turbo")
+
+        def retrieve(self, query_bundle):
+            nodes = self._retriever.retrieve(query_bundle)
+            tools = [self._object_node_mapping.from_node(n.node) for n in nodes]
+
+            sub_question_engine = SubQuestionQueryEngine.from_defaults(
+                query_engine_tools=tools, llm=self._llm
+            )
+            sub_question_description = f"""\
+    Useful for any queries that involve comparing multiple documents. ALWAYS use this tool for comparison queries - make sure to call this \
+    tool with the original query. Do NOT use the other tools for any queries involving multiple documents.
+    """
+            sub_question_tool = QueryEngineTool(
+                query_engine=sub_question_engine,
+                metadata=ToolMetadata(
+                    name="compare_tool", description=sub_question_description
+                ),
+            )
+
+            return tools + [sub_question_tool]
+
+
+custom_node_retriever = CustomRetriever(vector_node_retriever)
+
+# wrap it with ObjectRetriever to return objects
+custom_obj_retriever = CustomObjectRetriever(
+    custom_node_retriever, tool_mapping, all_tools, llm=llm
+)
+
+top_agent = FnRetrieverOpenAIAgent.from_retriever(
+    custom_obj_retriever,
+    system_prompt=""" \
+You are an agent designed to answer queries about the documentation.
+Please always use the tools provided to answer a question. Do not rely on prior knowledge.\
+
+""",
+    llm=llm,
+    verbose=True,
+)
 
 
 # Function to get the session state
@@ -235,13 +325,13 @@ def handle_input(conversation):
         # print(response)
         answer = get_response_without_metadata(response)
 
-        if 'processing' in st.session_state and not st.session_state['processing']:
-            # Add answer to conversation
-            conversation.append(("AI", answer))
-            # Clear input box
-            st.session_state['processing'] = False
-            st.session_state.question_input = ""
+        # if 'processing' in st.session_state and not st.session_state['processing']:
+        # Add answer to conversation
+        conversation.append(("AI", answer))
+        # Clear input box
+        st.session_state['processing'] = False
+        st.session_state.question_input = ""
 
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+if __name__ == "__main__":
+    main()
